d5eedbd737e564c20fd0dd90f7bbb509f6b60e50
Dave Flanagan <dflanaga@wiley.com>
2019-10-21 22:05:26 +0200

    First LDA .py.

---
+++ src/models/0.5-DWFlanagan-LDA.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import logging
+
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+
+logging.basicConfig(
+    filename="das.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+)
+
+
+def load_data():
+    logging.info(f"Proj root is {PROJ_ROOT}.")
+    load_path = os.path.join(PROJ_ROOT + "/data/processed/" + "tokenized.feather")
+
+    logging.info(f"Reading file from {load_path}.")
+
+    tokenized_statments = pd.read_feather(load_path)
+
+    logging.info(f"{len(tokenized_statments)} rows loaded.")
+
+    return tokenized_statments
+
+
+def vectorize(data_statements):
+    tfidf_vectorizer = CountVectorizer(min_df=5, max_df=0.9)
+    logging.info("Fitting TF-IDF.")
+    tfidf = tfidf_vectorizer.fit_transform(data_statements["proc_answers"])
+
+    return tfidf
+
+
+def build_lda_model(tfidf, n_topics=20):
+    lda = LatentDirichletAllocation(n_components=n_topics, random_state=7)
+    logging.info("Fitting LDA.")
+    topic_model = lda.fit(tfidf)
+
+    return topic_model
+
+
+PROJ_ROOT = os.path.join(os.curdir)
+
+tokenized_statments = load_data()
+tfidf = vectorize(tokenized_statments)
+topic_model = build_lda_model(tfidf)
+
